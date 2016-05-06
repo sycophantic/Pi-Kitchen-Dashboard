b3e6f0b8258b9c71761a6cf6b7e0500b0508a7ba
Dan <danielgrinkevich@gmail.com>
2016-05-05 20:48:34 -0400

    fixed 100% cpu issue with loop and added global vars

--- backlight.py
+++ backlight.py
@@ -9,18 +9,27 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.OUT)
 GPIO.output(23, 1)
 
+global oldtime
+global screen
 oldtime = time.time()
+screen = 1
 
 def motion(channel):
   print "turning screen on"
   GPIO.output(23, 1)
+  global oldtime
   oldtime = time.time()
+  global screen
+  screen = 1
 
 GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 GPIO.add_event_detect(24, GPIO.RISING, callback=motion, bouncetime=300)
 
 while True:
-  if time.time() - oldtime > 600:
-    print "turning screen off"
-    GPIO.output(23, 0)
+  time.sleep(1)
+  if time.time() - oldtime > 5:
+    if(screen):
+      print "turning screen off"
+      GPIO.output(23, 0)
+      screen = 0
